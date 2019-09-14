9bacdda529572ffbf96f6ee19444108d87178a1f
gautam <jarvis.gomzi@gmail.com>
2019-09-14 18:40:17 +0530

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello guys\n');
+  res.end('Hello! i made a changes\n');
 });
 
 server.listen(port, hostname, () => {
